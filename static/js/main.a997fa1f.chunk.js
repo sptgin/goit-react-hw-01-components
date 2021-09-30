24ae90eb0c0def3127e63323238b239681b35474
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-09-30 16:42:39 +0300

    Updates

---
+++ static/js/main.a997fa1f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);var c=e(1),o=e.n(c),r=e(3),i=e.n(r),s=(e(8),e(9),e(0));var h=function(){return Object(s.jsx)("div",{children:Object(s.jsx)("h1",{children:"HW 001"})})};i.a.render(Object(s.jsx)(o.a.StrictMode,{children:Object(s.jsx)(h,{})}),document.getElementById("root"))},8:function(t,n,e){},9:function(t,n,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.a997fa1f.chunk.js.map
