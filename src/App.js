f0fa2a781bed2cf0ef7a244cb3dc0c44fd37b3b1
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-10-03 15:20:36 +0300

    add section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 
+//import PaintingList from './components/PaintingList';
+import Section from './components/Section';
+import Profile from './components/Section'
+
 import './App.css';
 
 function App() {
-  return ( <div><h1>HW 001</h1></div>
+  return ( 
+    <div>
+    <h1>React HW 001</h1>
+
+    <Section title="Task 001">
+      <Profile></Profile>
+    </Section>
+    <Section title="Task 002"></Section>
+    <Section title="Task 003"></Section>
+    <Section title="Task 004"></Section>
+
+    </div>
+
  );
 }
 
 export default App;
+
+
+
+
